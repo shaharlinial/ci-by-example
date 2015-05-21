4dee4e9116bc707f2388c27b6b9ab6eed782f26e
shaharpinthedark <shaharlini@gmail.com>
2015-05-21 14:58:19 +0300

    Playing with CI

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app = http.createServer(handler);
 app.listen(8080);
 
 function handler (req, res) {
+  console.log('test this push request');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('It\'s alive!');
 }
